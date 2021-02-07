e5e3c81e6cc241d8603cad432acd69fe331d10d9
Mir Hussain <mir.hm110@gmail.com>
2021-02-07 19:48:15 +0600

    final update

--- script.js
+++ script.js
@@ -1,31 +1,86 @@
 const searchButton = document.querySelector("#search-button").addEventListener("click", () => {
     const foodName = document.querySelector("#search").value;
+    if (foodName === "") {
+        return null;
+    } else {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s= ${foodName}`)
+            .then((res) => res.json())
+            .then((data) => displayFood(data));
 
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s= ${foodName}`)
-        .then((res) => res.json())
-        .then((data) => displayFood(data));
+        const displayFood = (foods) => {
+            if (foods.meals === null) {
+                alert("We do not have this recipe. Please try again.");
+            } else {
+                foods.meals.forEach((food) => {
+                    const foodsContainer = document.querySelector("#foods-container");
 
-    const displayFood = (foods) => {
-        for (let i = 0; i < foods.meals.length; i++) {
-            const food = foods.meals[i].strMeal;
-            const h3 = document.createElement("h3");
-            h3.className = "food-name"
-            h3.innerText = food;
-            const foodContainer = document.querySelector("#food-container");
-            foodContainer.appendChild(h3);
-        }
-    };
+                    const foodContainer = document.createElement("div");
+                    foodContainer.className = "food";
+                    foodsContainer.appendChild(foodContainer);
+
+                    const foodCover = `
+                <div onclick="detailedInfo('${food.idMeal}')">
+                    <img src="${food.strMealThumb}">
+                    <h3> ${food.strMeal} </h3>
+                </div>
+            `;
+                    foodContainer.innerHTML = foodCover;
+                });
+
+                document.querySelector("#search").value = "";
+            }
+        };
+    }
 });
 
-const foodContainer = document.querySelector("#food-container").addEventListener("click", (e) => {
-    console.log(e.target.innerText);
-})
+const detailedInfo = (foodId) => {
+    const fullDetails = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`;
+    fetch(fullDetails)
+        .then((res) => res.json())
+        .then((data) => details(data.meals[0]));
+};
 
-// fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
-// .then(res => res.json())
-// .then(data => document.getElementById("food-container").innerText = data.meals[0].strMeal)
+const details = (foodInfo) => {
+    const info = `
+        <div id="info-container">
+            <img src="${foodInfo.strMealThumb}">
+            <div id="dish-info">
+            <h2>${foodInfo.strMeal}</h2>
+                <div id="ingredient">
+                    <div>
+                        <ul>
+                            <li>${foodInfo.strMeasure1} ${foodInfo.strIngredient1} </li>
+                            <li>${foodInfo.strMeasure2} ${foodInfo.strIngredient2} </li>
+                            <li>${foodInfo.strMeasure3} ${foodInfo.strIngredient3} </li>
+                            <li>${foodInfo.strMeasure4} ${foodInfo.strIngredient4} </li>
+                            <li>${foodInfo.strMeasure5} ${foodInfo.strIngredient5} </li>
+                            <li>${foodInfo.strMeasure6} ${foodInfo.strIngredient6} </li>
+                            <li>${foodInfo.strMeasure7} ${foodInfo.strIngredient7} </li>
+                            <li>${foodInfo.strMeasure8} ${foodInfo.strIngredient8} </li>
+                            <li>${foodInfo.strMeasure9} ${foodInfo.strIngredient9} </li>
+                            <li>${foodInfo.strMeasure10} ${foodInfo.strIngredient10} </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>${foodInfo.strMeasure11} ${foodInfo.strIngredient11} </li>
+                            <li>${foodInfo.strMeasure12} ${foodInfo.strIngredient12} </li>
+                            <li>${foodInfo.strMeasure13} ${foodInfo.strIngredient13} </li>
+                            <li>${foodInfo.strMeasure14} ${foodInfo.strIngredient14} </li>
+                            <li>${foodInfo.strMeasure15} ${foodInfo.strIngredient15} </li>
+                            <li>${foodInfo.strMeasure16} ${foodInfo.strIngredient16} </li>
+                            <li>${foodInfo.strMeasure17} ${foodInfo.strIngredient17} </li>
+                            <li>${foodInfo.strMeasure18} ${foodInfo.strIngredient18} </li>
+                            <li>${foodInfo.strMeasure19} ${foodInfo.strIngredient19} </li>
+                            <li>${foodInfo.strMeasure20} ${foodInfo.strIngredient20} </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    const detailsSection = document.getElementById("details-section");
+    detailsSection.innerHTML = info;
+};
 
-// data.meals[0].strMeal
-// const div = document.createElement("div");
-// const currentDiv = document.getElementById("food-container");
-// currentDiv.appendChild(div);
+document.querySelector("#search").value = "";
